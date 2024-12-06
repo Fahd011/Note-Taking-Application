d711023e092d1f2cec90f50a60ce0354b9497509
Fahd011 <131470949+Fahd011@users.noreply.github.com>
2024-12-06 16:04:51 +0500

    Revert "changed port number and local host urls"

--- frontend/src/constants/userConstants.jsx
+++ frontend/src/constants/userConstants.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "https://note-taker-app.azurewebsites.net/api";
+const BASE_URL = "http://localhost:5000/api";
 
 export const USER_ROUTES = {
   SIGN_UP: `${BASE_URL}/users/signup`,
