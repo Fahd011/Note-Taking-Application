776ba1209883a5c6e1f3987582dd873d37ccb3a3
Mohmmad Anas <anas1.dev@gmail.com>
2024-12-06 23:58:46 +0500

    refactored

--- aws-terraform.tf
+++ aws-terraform.tf
@@ -1,30 +1,37 @@
-# Provider Configuration
 provider "aws" {
   region = "us-west-2"
 }
 
-# Key Pair
-resource "aws_key_pair" "deploy_key" {
-  key_name   = "mern-app-key"
-public_key = file("/home/lenovo/.ssh/id_ed25519.pub")
+resource "aws_key_pair" "mern_key" {
+  key_name   = "mern-key"
+  public_key = file("/home/lenovo/.ssh/id_ed25519.pub")
 }
 
-# Security Group
 resource "aws_security_group" "mern_sg" {
   name        = "mern-sg"
-  description = "Allow HTTP, HTTPS, and SSH traffic"
+  description = "Allow SSH, HTTP, and DB connections"
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 27017 # MongoDB default port
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -33,53 +40,21 @@ resource "aws_security_group" "mern_sg" {
   }
 }
 
-# EC2 Instances (Blue and Green)
-resource "aws_instance" "blue" {
-  ami           = "ami-055e3d4f0bbeb5878" # Example Amazon Linux 2 AMI
-  instance_type = "t2.micro" # Changed from t2.micro to t3.micro
-  key_name      = aws_key_pair.deploy_key.key_name
-  security_groups = [aws_security_group.mern_sg.name]
-  tags = {
-    Name = "blue-instance"
-        Environment = "Blue"
+resource "aws_instance" "mern_server" {
+  ami           = "ami-05d38da78ce859165" 
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.mern_key.key_name
+  vpc_security_group_ids = [aws_security_group.mern_sg.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install -y docker.io docker-compose
+              mkdir /home/ubuntu/mern-docker
+              EOF
+
+  tags = {
+    Name = "mern-server"
   }
 }
 
-resource "aws_instance" "green" {
-  ami           = "ami-055e3d4f0bbeb5878" # Example Amazon Linux 2 AMI
-  instance_type = "t2.micro" # Changed from t2.micro to t3.micro
-  key_name      = aws_key_pair.deploy_key.key_name
-  security_groups = [aws_security_group.mern_sg.name]
-  tags = {
-    Name = "green-instance"
-        Environment = "Green"
-
-  }
-}
-
-# Elastic Load Balancer
-resource "aws_elb" "mern_lb" {
-  name               = "mern-load-balancer"
-  availability_zones = ["us-west-2a", "us-west-2b"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "HTTP"
-    lb_port           = 80
-    lb_protocol       = "HTTP"
-  }
-
-  instances = [aws_instance.blue.id] # Initially point to blue instance
-  health_check {
-    target              = "HTTP:80/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "MERN Load Balancer"
-  }
-}
