4a796415b3b98c3239c372656c8e37c14160f92a
Fahd011 <fahdaleem256@gmail.com>
2024-12-10 18:24:36 +0500

    changed login title

--- frontend/src/screens/LoginPage/LoginPage.jsx
+++ frontend/src/screens/LoginPage/LoginPage.jsx
@@ -61,7 +61,7 @@ const LoginPage = () => {
   };
 
   return (
-    <MainScreen title="LOGIN on v7">
+    <MainScreen title="LOGIN">
       <div className="login-container">
         <Form onSubmit={submitHandler}>
           {error && <Alert variant="danger">{error}</Alert>}
